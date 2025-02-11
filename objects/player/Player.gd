1226f7bc32f8278f62cf8b932df28824a5da2498
Arvan <arvanwijaya01@gmail.com>
2025-02-11 09:03:38 +0700

    Implement skeleton to Player

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-var position_datalist = []
-var index_count = 0
-var latest_index = 0
+var mouse_position = Vector2.ZERO
 var move_vec = Vector2.ZERO
+onready var skeleton = $HumanSkeleton
+onready var animation_player = $HumanSkeleton/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,28 +11,28 @@ func _ready():
 
 
 func _physics_process(_delta):
-	if !Input.is_action_pressed("reverse_time"):
-		$CanvasLayer/ColorRect.material.set_shader_param("invert", false)
-		move_vec = move_and_slide(move_vec, Vector2.UP)
-		move_vec.y += 10
-		var target_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-		if is_on_floor():
-			move_vec.x = clamp(move_vec.x + target_dir * 16, -256, 256)
-			if target_dir == 0:
-				move_vec.x = lerp(move_vec.x, 0, 0.2)
-		elif target_dir != 0:
-			move_vec.x = clamp(move_vec.x + target_dir * 16, -256, 256)
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			move_vec.y = -512
-		if position_datalist.size() < 500:
-			position_datalist.append([position, move_vec])
-		else:
-			position_datalist[index_count] = [position, move_vec]
-		latest_index = index_count
-		index_count += 1
+	mouse_position = get_global_mouse_position()
+	if mouse_position.x > position.x:
+		skeleton.scale = Vector2(1.0, 1.0)
+	elif mouse_position.x < position.x:
+		skeleton.scale = Vector2(-1.0, 1.0)
+	# Movement
+	move_vec = move_and_slide(move_vec, Vector2.UP)
+	move_vec.y += 10
+	var target_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if abs(int(target_dir + skeleton.scale.x)) == 2:
+		move_vec.x = clamp(move_vec.x + target_dir * 15, -60, 60)
+		if animation_player.current_animation != "WalkForward":
+			animation_player.play("WalkForward")
+	elif abs(int(target_dir + skeleton.scale.x)) == 0:
+		move_vec.x = clamp(move_vec.x + target_dir * 15, -40, 40)
+		if animation_player.current_animation != "WalkBackward":
+			animation_player.play("WalkBackward")
 	else:
-		$CanvasLayer/ColorRect.material.set_shader_param("invert", true)
-		if position_datalist.size() > 0:
-			var prev_condition = position_datalist.pop_back()
-			position = prev_condition[0]
-			move_vec = prev_condition[1]
+		if animation_player.current_animation != "Idle":
+			animation_player.play("Idle")
+	if target_dir == 0:
+		move_vec.x = lerp(move_vec.x, 0, 0.2)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		move_vec.y = -512
+	
