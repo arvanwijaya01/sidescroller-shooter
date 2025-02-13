6033140fcb162b5b091d9cd67d9b919b69df3fc1
Arvan <arvanwijaya01@gmail.com>
2025-02-13 08:03:34 +0700

    Add Pistol recoil

--- objects/gun/Pistol.gd
+++ objects/gun/Pistol.gd
@@ -11,13 +11,6 @@ onready var line = $RayCast2D/Node/Line2D
 func _ready():
 	pass # Replace with function body.
 
-
-#func _physics_process(delta):
-#	if Input.is_action_just_pressed("shoot"):
-#		shoot()
-#	if Input.is_action_just_pressed("reload"):
-#		reload()
-
 func shoot():
 	if !animation_player.is_playing() and ammo_in_magazine != 0:
 		line.points[1] = Vector2(((raycast.get_collision_point() - raycast.global_position).length()) if raycast.is_colliding() else 1000.0, 0.0)
@@ -26,6 +19,11 @@ func shoot():
 		line.position = raycast.global_position
 		line.scale = scale
 		animation_player.play("Shoot")
+		var viewport_size = get_viewport_rect().size
+		var recoil_position = Vector2(rand_range(-5.0, 5.0), rand_range(-10.0, -5.0))
+		recoil_position = get_viewport().get_mouse_position() + recoil_position.rotated(rotation)
+		recoil_position = Vector2(clamp(recoil_position.x, 0.0, viewport_size.x), clamp(recoil_position.y, 0.0, viewport_size.y))
+		get_viewport().warp_mouse(recoil_position)
 
 func reload():
 	if !animation_player.is_playing() and ammo_in_magazine < 15:
